24e90f38b9061e61229780e490443bb41343fe68
Dong gyo Jeong <ehdrylang@naver.com>
2020-01-02 20:56:58 +0900

    init springboot application

---
+++ TestPractice/src/main/java/com/tistory/jeongpro/TestPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.jeongpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPracticeApplication.class, args);
+	}
+
+}
---
+++ TestPractice/src/test/java/com/tistory/jeongpro/TestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.jeongpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
