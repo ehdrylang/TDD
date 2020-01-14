d17893d23966645a75239101cf5b1a17e6e989d2
Dong gyo Jeong <ehdrylang@naver.com>
2020-01-14 23:00:52 +0900

    add matches method test

--- src/test/java/com/tistory/jeongpro/string/StringApiTests.java
+++ src/test/java/com/tistory/jeongpro/string/StringApiTests.java
@@ -299,4 +299,16 @@ public class StringApiTests {
 		}
 		assertEquals(3, stringArray.length);
 	}
+	@Test
+	public void matches_existChar_returnTrue(){
+		assertTrue(firstName.matches("p"));
+	}
+	@Test
+	public void matches_notExistChar_returnFalse(){
+		assertFalse(firstName.matches("hello"));
+	}
+	@Test
+	public void matches_existRegexIpPattern_returnTrue(){
+		assertTrue("http://192.168.0.111:8080".matches("(?:https?)://(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]):(6553[0-5]|655[0-2][0-9]|65[0-4][0-9][0-9]|6[0-4][0-9][0-9][0-9]|\\d{2,4}|[1-9]).*"));
+	}
 }
