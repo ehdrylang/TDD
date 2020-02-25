08122ab9bf6ba1998e3e43d52b0f870e7ce517e6
Dong gyo Jeong <ehdrylang@naver.com>
2020-02-25 23:17:54 +0900

    frame update

--- src/main/java/com/tistory/jeongpro/bowling/Frame.java
+++ src/main/java/com/tistory/jeongpro/bowling/Frame.java
@@ -1,7 +1,42 @@
 package com.tistory.jeongpro.bowling;
 
-public class Frame extends BaseFrame {
-    public Frame(){
-        super(2);
+
+public class BaseFrame {
+    protected int pins;
+    protected int score[];
+    private int idx;
+
+    public BaseFrame(){
+        this.pins = 10;
+        this.score = new int[]{-1, -1};
+        this.idx = 0;
+    }
+    public void bowl(int count){
+        //validation
+        if(!isValidBowl(count)){
+            throw new RuntimeException("Invalid count");
+        }
+        //end
+        if(isEnd()){
+            throw new RuntimeException("You can't proceed any more");
+        }
+        pins -= count;
+        score[idx] = count;
+        idx++;
+    }
+    public boolean isValidBowl(int count){
+        return count >= 0 && count <= 10 && (pins - count) >= 0;
+    }
+    public boolean isStrike() {
+        return score[0] == 10;
+    }
+    public boolean isSpare() {
+        return score[0] != 10 && score[0] + score[1] == 10;
+    }
+    public boolean isEnd(){
+        return pins == 0 || idx > 1;
+    }
+    public int getScore(int bowl){
+        return score[bowl];
     }
 }
